2463df2afc7aa63ff37be49fa00dbd0573ca8223
alexinkingb <143071031+alexinkingb@users.noreply.github.com>
2024-04-06 17:39:38 +0300

    06.04.24 js

--- script.js
+++ script.js
@@ -1,31 +1,53 @@
-let prices = document.getElementsByClassName("products-item-price");
-document.getElementById("change-currency").onclick = function (e){
-    let currentCurrency = e.target.innerText;
+const nav = document.querySelector('.navigation');
 
-    let newCurrency = "$";
-    let coefficient = 1;
+nav.addEventListener('click', function (event) {
+    // on which element the click was
+    let clickTarget = event.target;
+    
+    let header = document.querySelector('.header');
+    let main_info = document.querySelector('.main-info');
+    let main__news_block = document.querySelector('.main__news-block');
+    let about_the_authors = document.querySelector('.about-the-authors');
+    let news_text__desc_1 = document.querySelector('.news-text__desc-1');
+    let news_text__desc_2 = document.querySelector('.news-text__desc-2');
+    let news_text__desc_3 = document.querySelector('.news-text__desc-3');
 
-    if (currentCurrency === "$") {
-        newCurrency = "₽";
-        coefficient = "80";
-    }else if (currentCurrency === "₽") {
-        newCurrency = "BYN";
-        coefficient = "3";
-    }
-    else if (currentCurrency === "BYN") {
-        newCurrency = "AUD";
-        coefficient = "58";
-    }
-    else if (currentCurrency === 'AUD') {
-        newCurrency = '€';
-        coefficient = 0.9;
-    } else if (currentCurrency === '€') {
-        newCurrency = '¥';
-        coefficient = 6.9;
-    }
-        e.target.innerText = newCurrency;
+    // searching an element with a class 'active-border' in the iPhone card
+    // let activeButtonColor = iPhone.querySelector('.active-border');
 
-    for (let i = 0; i < prices.length; i++) {
-    prices[i].innerText = +(prices[i].getAttribute("data-base-price") * coefficient).toFixed(1) + " " + newCurrency;
-}
-}
+    // creating a variable for a pack color
+    // let packColor = iPhone.querySelector('.pack');
+
+    // the FUNCTION of the colour changing
+    if (clickTarget.classList.contains('themetoggle') && 
+        !clickTarget.classList.contains('dark')) {
+            clickTarget.classList.add('dark');
+    }
+
+    // the FUNCTION of the changing of gigobites-button colour
+    if (clickTarget.classList.contains('themetoggle') && 
+        clickTarget.classList.contains('dark')) {
+            header.classList.add('header-dark');
+            header.classList.remove('header');
+            
+            main_info.classList.add('main-info-dark');
+            main_info.classList.remove('main-info');
+            
+            main__news_block.classList.add('main__news-block-dark');
+            main__news_block.classList.remove('main__news-block');
+            
+            about_the_authors.classList.add('about-the-authors-dark');
+            about_the_authors.classList.remove('about-the-authors');
+
+            news_text__desc_1.classList.add('.news-text__desc-1-dark');
+            news_text__desc_1.classList.remove('.news-text__desc-1');
+
+            news_text__desc_2.classList.add('.news-text__desc-2-dark');
+            news_text__desc_2.classList.remove('.news-text__desc-2');
+
+            news_text__desc_3.classList.add('.news-text__desc-3-dark');
+            news_text__desc_3.classList.remove('.news-text__desc-3');
+    }
+
+    
+})
